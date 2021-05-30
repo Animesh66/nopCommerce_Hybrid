6840f6b96c7fbf4715373af0e01a2fb0c339fdd4
Animesh <er.animesh6@hotmail.com>
2021-05-30 22:47:28 +0530

    login test is COMPLETED

--- test_cases/test_login.py
+++ test_cases/test_login.py
@@ -3,6 +3,7 @@
 from page_object.login_page import LoginNopCommerce
 from test_cases.config_test import setup
 
+
 class TestTC001Login:
     base_url = "https://admin-demo.nopcommerce.com"
     email = "admin@yourstore.com"
