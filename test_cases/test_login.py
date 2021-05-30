9e9ee2f2f88675e08477b7713f16e3633a161b75
Animesh <er.animesh6@hotmail.com>
2021-05-30 22:32:07 +0530

    login test is COMPLETED

--- test_cases/test_login.py
+++ test_cases/test_login.py
@@ -1,9 +1,8 @@
 # this testcase will perform the login functionality testing
 # filename should always start with test as pytest have this naming convention
-from selenium import webdriver
-import time
-import pytest
 from page_object.login_page import LoginNopCommerce
+from selenium import webdriver
+import pytest
 
 
 class TestTC001Login:
@@ -11,19 +10,25 @@ class TestTC001Login:
     email = "admin@yourstore.com"
     password = "admin"
 
-    def test_homepage_title(self, setup):
-        self.driver = setup
+
+    def test_homepage_title(self):
+        self.driver = webdriver.Chrome()
+        # self.driver = setup
         self.driver.maximize_window()
         self.driver.get(self.base_url)
         actual_title = self.driver.title
-        if actual_title == "Your store. Login":
+        if actual_title == "Your store. Login123":
+            self.driver.close()
             assert True
         else:
+            self.driver.save_screenshot("./screenshots/test_homepage_title.png")  # this is the path and screenshot name
+            self.driver.close()
             assert False
-        self.driver.close()
 
-    def test_login(self, setup):
-        self.driver = setup
+
+    def test_login(self):
+        # self.driver = setup
+        self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get(self.base_url)
         self.login = LoginNopCommerce(self.driver)
@@ -31,8 +36,10 @@ class TestTC001Login:
         self.login.set_password(self.password)
         self.login.click_login()
         actual_title = self.driver.title
-        if actual_title == "Dashboard / nopCommerce administration":
+        if actual_title == "Dashboard / nopCommerce administration123":
+            self.driver.close()
             assert True
         else:
+            self.driver.save_screenshot("./screenshots/screenshotstest_login.png")  # this is the path and screenshot name
+            self.driver.close()
             assert False
-        self.driver.close()
