3a7c0876cb12b7873c9abe7369f3d966fc5b4163
Animesh <er.animesh6@hotmail.com>
2021-05-31 21:39:33 +0530

    login test is COMPLETED

---
+++ utilities/cutom_logger.py
@@ -0,0 +1,13 @@
+import logging
+
+
+# this file will generate INFO lo for test cases
+class LogGeneration:
+    @staticmethod
+    def log_generation():
+        logging.basicConfig(filename="./logs/automation.log",
+                            format='%(asctime)s: %(levelname)s : %(message)s ',
+                            datefmt='%m/%d/%Y %I:%M:%S %p')
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        return logger
