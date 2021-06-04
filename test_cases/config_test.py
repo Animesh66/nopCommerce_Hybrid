a073233b7738d92ec424de4dcc27375be2f2a85b
Animesh <er.animesh6@hotmail.com>
2021-06-04 20:05:27 +0530

    login Data Driven Test is COMPLETED

--- test_cases/config_test.py
+++ test_cases/config_test.py
@@ -4,7 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 # this file will automatically download the latest browser driver and run the executable file.
 # Syntax is driver = webdriver.Chrome(ChromeDriverManager().install())
 # To install webdriver-manager use below command in Terminal
-# pip3 install webdriver-manager
+# pip3 install webdriver-manager in MAC
 
 
 @pytest.fixture()
